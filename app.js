a6062ee70ac78ce3adc492c38722f7beaea4322c
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-12-12 16:53:10 +0100

    'hai

--- app.js
+++ app.js
@@ -8,14 +8,34 @@ const mediumUrl = 'https://medium.com/feed/@fitnessspace.ng';
 const parser = new RSSParser();
 let articles = [];
 
+// const parse = async url => {
+//   try {
+//     const feed = await parser.parseURL(url);
+//     console.log(feed)
+//     articles.push(feed)
+//     // feed.items.map(item => {
+//       // articles.push({ item });
+//     // });
+//   } catch (err) {
+//     console.log(err.message);
+//     throw err;
+//   }
+// };
+
+const CACHE_TIME = 60 * 60 * 1000; // 1 hour
+let lastFetchTime = 0;
+
 const parse = async url => {
   try {
-    const feed = await parser.parseURL(url);
-    console.log(feed)
-    articles.push(feed)
-    // feed.items.map(item => {
-      // articles.push({ item });
-    // });
+    const currentTime = new Date().getTime();
+    // Check if it's been more than CACHE_TIME milliseconds since the last fetch
+    if (currentTime - lastFetchTime > CACHE_TIME) {
+      const feed = await parser.parseURL(url);
+      feed.items.forEach(item => {
+        articles.push({ item });
+      });
+      lastFetchTime = currentTime; // Update the last fetch time
+    }
   } catch (err) {
     console.log(err.message);
     throw err;
