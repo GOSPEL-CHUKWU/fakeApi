609a0fcb9021a68cb4a1d8fea50ce5d1d03507bd
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-12-12 10:10:44 +0100

    andy

---
+++ app.js
@@ -0,0 +1,29 @@
+import cors from 'cors';
+import express from 'express';
+import RSSParser from 'rss-parser';
+
+const mediumUrl = 'https://medium.com/feed/@fitnessspace.ng';
+
+const parser = new RSSParser();
+let articles = [];
+
+const parse = async url => {
+  const feed = await parser.parseURL(url);
+  feed.items.forEach(item => {
+    articles.push({ item });
+  });
+};
+parse(mediumUrl);
+
+let app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send(articles);
+});
+
+const server = app.listen('4000', () => {
+  console.log('App is listening at http://localhost:4000');
+});
+
+export default server;
